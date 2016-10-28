2c1b25c32115a08035cbe1d79afacb2cb7c00698
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-10-28 12:30:34 +0800

    Function:add show

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,7 +1,18 @@
 class DinnersController < ApplicationController
+	before_action :find_dinner, :only =>:show
+
 	def index
 		# @dinners=Dinner.all
 		@dinners=Dinner.page(params[:page]).per(5)
 	end
 
+	def show		
+		
+	end
+
+	private
+
+	def find_dinner
+		@dinner=Dinner.find(params[:id])
+	end
 end

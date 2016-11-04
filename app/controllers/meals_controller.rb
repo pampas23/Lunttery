ccf9ade817e27f629bb1b9a9b08ce774309a7942
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-04 21:11:55 +0800

    Function:Create meal in dinner show page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,4 +3,24 @@ class MealsController < ApplicationController
 	def new
 		@meal=Meal.new	
 	end
+
+	def create
+		@dinner=Dinner.find_by(params[:id])
+		@meal=Meal.new(meal_params)
+		@meal.dinner=@dinner
+		if @meal.save(meal_params)
+			flash[:notice]="meal saved successfully"
+			redirect_to dinner_path(@dinner)
+		else
+			flash[:alert]="meal not saved "
+			render 'show'
+		end		
+	end
+
+private
+
+def meal_params
+	params.require(:meal).permit(:name,:discription,:style_id,:price)
+end
+
 end

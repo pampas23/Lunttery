5efdcc34a963d83e8aeebcc6e2c329080c3ef020
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-09 14:04:40 +0800

    Bug fix : delegate

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,10 @@ class Meal < ApplicationRecord
 	belongs_to :dinner
 	belongs_to :style
 	validates_presence_of :name
+	delegate :name, :to => :meal, :prefix => true, :allow_nil => true
+	delegate :discription, :to => :meal, :prefix => true, :allow_nil => true
+	delegate :price, :to => :meal, :prefix => true, :allow_nil => true
+	delegate :style_id, :to => :meal, :prefix => true, :allow_nil => true
 
 	has_many :photos, :as => :phototable
 	accepts_nested_attributes_for :photos

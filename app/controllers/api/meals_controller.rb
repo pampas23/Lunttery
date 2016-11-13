8913107c3ca9495f08de08e2041ed7b6c0def9d6
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-13 14:11:22 +0800

    Test : API test with iOS

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -7,28 +7,28 @@ class Api::MealsController < Api::ApiController
 				:data => @meal.return_json
 			}
 		else
-			@lat = params[:lat]
-			@lng = params[:lng]
-			@distant = params[:distant]
-			@style_ids = params[:style_ids].split(",").map { |s| s.to_i }
-			# @distant = 0.1
-			# @style_ids = [1,4]
-			# @location = Geokit::LatLng.new(25.0521723,121.5321898)
-			@location = Geokit::LatLng.new(params[:lat],params[:lng])
-			@dinners = Dinner.within(@distant,:origin => @location).includes(:meals)
+			# @lat = params[:lat]
+			# @lng = params[:lng]
+			# @distant = params[:distant]
+			# @style_ids = params[:style_ids].split(",").map { |s| s.to_i }
+			# # @distant = 0.1
+			# # @style_ids = [1,4]
+			# # @location = Geokit::LatLng.new(25.0521723,121.5321898)
+			# @location = Geokit::LatLng.new(params[:lat],params[:lng])
+			# @dinners = Dinner.within(@distant,:origin => @location).includes(:meals)
 			
-			@select_meals =[]
-			@meals = @dinners.flat_map{ |d| d.meals }
+			# @select_meals =[]
+			# @meals = @dinners.flat_map{ |d| d.meals }
 
-			@meals.each do |meal|
-				@style_ids.each do |style_id|
-					if meal[:style_id] == style_id
-						@select_meals << meal
-					end
-				end
-			end
+			# @meals.each do |meal|
+			# 	@style_ids.each do |style_id|
+			# 		if meal[:style_id] == style_id
+			# 			@select_meals << meal
+			# 		end
+			# 	end
+			# end
 
-
+			@select_meals=Meal.all
 			
 
 			if current_user

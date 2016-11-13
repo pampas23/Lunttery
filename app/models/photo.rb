7ba18717fc7d3fb54ff86b79864e682d12cc5016
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-13 15:10:54 +0800

    Function : adjust photo upload medium size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "375x275>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 	belongs_to :phototable, :polymorphic => true

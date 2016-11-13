3a8f7ea266a5bfdc6a751ab2d522f7d1e37a25a1
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-13 14:23:52 +0800

    Bug fix : fix demo lat and lng

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -7,29 +7,25 @@ class Api::MealsController < Api::ApiController
 				:data => @meal.return_json
 			}
 		else
-			# @lat = params[:lat]
-			# @lng = params[:lng]
-			# @distant = params[:distant]
-			# @style_ids = params[:style_ids].split(",").map { |s| s.to_i }
-			# # @distant = 0.1
-			# # @style_ids = [1,4]
-			# # @location = Geokit::LatLng.new(25.0521723,121.5321898)
-			# @location = Geokit::LatLng.new(params[:lat],params[:lng])
-			# @dinners = Dinner.within(@distant,:origin => @location).includes(:meals)
-			
-			# @select_meals =[]
-			# @meals = @dinners.flat_map{ |d| d.meals }
+			@lat = params[:lat]
+			@lng = params[:lng]
+			@distant = params[:distant]
+			@style_ids = params[:style_ids].split(",").map { |s| s.to_i }
 
-			# @meals.each do |meal|
-			# 	@style_ids.each do |style_id|
-			# 		if meal[:style_id] == style_id
-			# 			@select_meals << meal
-			# 		end
-			# 	end
-			# end
-
-			@select_meals=Meal.all
+			@location = Geokit::LatLng.new(params[:lat],params[:lng])
+			@dinners = Dinner.within(@distant,:origin => @location).includes(:meals)
 			
+			@select_meals =[]
+			@meals = @dinners.flat_map{ |d| d.meals }
+
+			@meals.each do |meal|
+				@style_ids.each do |style_id|
+					if meal[:style_id] == style_id
+						@select_meals << meal
+					end
+				end
+			end
+
 
 			if current_user
 				my_like_meal_ids = current_user.user_meal_likeships.pluck(:meal_id)

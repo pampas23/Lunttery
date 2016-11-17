95845d4ddacab7e667640273f6d1a65c4831e443
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-17 16:32:01 +0800

    API : add distant info in users liked list

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,17 @@
 class Api::UsersController < Api::ApiController
 	def show
 		@user=User.find(params[:id])
+		@lat = params[:lat]
+		@lng = params[:lng]
+		@location = Geokit::LatLng.new(params[:lat],params[:lng])
+
 		if @user
 			render :json => {
-					:data => @user.meals.map{|meal| meal.return_json}
+					:data => @user.meals.map{|meal| meal.return_json
+					h = meal.return_json
+					h[:distant]=@location.distance_from(meal.dinner , :units => :kms)
+					h
+					}
 				}
 		else
 			render :json => {

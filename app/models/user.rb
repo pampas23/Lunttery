b676877cfe708a6cc024ddc26bc5801962f3ab1e
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-17 15:29:27 +0800

    API : user can fb log in api

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,7 @@ class User < ApplicationRecord
   end
   
   def self.get_fb_data(access_token)
-    res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :access_token => access_token } }
-
+    res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :access_token => access_token, :fields => "id,name,email" } }
     if res.code == 200
       JSON.parse( res.to_str )
     else

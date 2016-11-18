bdd2bb4dd2ee2c4b8d72c8a2a2dabfdaf2446c6c
Pampas <HaoYi@chenhaoyide-MacBook-Air.local>
2016-11-18 13:42:10 +0800

    API : can’t use coupon when diner is no on sale

--- app/controllers/api/coupon_tracks_controller.rb
+++ app/controllers/api/coupon_tracks_controller.rb
@@ -5,37 +5,47 @@ class Api::CouponTracksController < Api::ApiController
 
 		if @user&&@dinner
 			@sale = false
-			@message = "一天只能領取一次喔"
+			@message = "目前沒有優惠喔"
 
-			if @user.coupon_tracks == []
-				@user.coupon_used << @dinner
-				@sale = true
-				@message = "領取餐券成功"
-
-			else 
-				@cd_hour = (Time.now - @user.coupon_tracks.last.created_at)/3600
-				if @cd_hour < 16
-
-				else
+			if @dinner.onsale == "true"
+				@message = "領取餐券成功"	
+				
+				if @user.coupon_tracks == []
 					@user.coupon_used << @dinner
-					@sale = true
-					@message = "領取餐券成功"
-				end			
-			end
+					@sale = true					
+					@last_coupon_used = nil
+				else 
+					@cd_hour = (Time.now - @user.coupon_tracks.last.created_at)/3600
+					@last_coupon_used = @user.coupon_tracks.last.created_at
+					
+					if @cd_hour < 20
+						@message = "一天只能領一次喔"
+					else
+						@user.coupon_used << @dinner
+						@sale = true
+					end
+				end
 
-			render :json => {
-				:can_use_cupon => @sale,
-				:message =>@message,
-				:user => @user,
-				:dinner => @dinner,
-				:last_used_coupon => @user.coupon_tracks.where(:dinner => @dinner).last,
-				:last_coupon_used => @user.coupon_tracks.last.created_at,
-				:past_time => @cd_hour,
-			}
-		else
-			render :json => {
-				:data => "no data"
-			}
+				render :json => {
+					:can_use_cupon => @sale,
+					:message =>@message,
+					:user => @user,
+					:dinner => @dinner,
+					:last_used_coupon => @user.coupon_tracks.where(:dinner => @dinner).last,
+					:last_coupon_used => @last_coupon_used,
+					:past_time => @cd_hour,
+				}
+			else
+				render :json => {
+					:can_use_cupon => @sale,
+					:message =>@message,
+					:user => @user,
+					:dinner => @dinner,
+					:last_used_coupon => nil,
+					:last_coupon_used => nil,
+					:past_time => nil,
+				}
+			end
 		end
 	end
 end
